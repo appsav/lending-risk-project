3ee8d7b02e518e9b847eded0dc4ee0ed0d0fffdd
appsav <appsavallos@gmail.com>
2024-10-01 17:09:05 -0600

    Essential Files
    This contains the primary python program for parsing and optimizing models as well as the actual model implementation I built.

---
+++ data engineering.py
@@ -0,0 +1,286 @@
+# -*- coding: utf-8 -*-
+
+
+import pandas as pd
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+import itertools
+import time
+from neural_net import net
+
+warnings.filterwarnings('ignore')
+		
+		
+
+####==============================================####
+####============== Data Exploration ==============####
+####==============================================####
+
+def print_records(_id, dataset):
+	print("============================================")
+	for row_num, row in enumerate(dataset[dataset["SK_ID_CURR"]==_id].values):
+		for col_num, column in enumerate(dataset.columns):
+			print("{}: {}".format(column, row[col_num]))
+		print("")
+	print("============================================")
+		
+def convert_frame(frame):
+	frame = frame.astype(float)
+	return torch.tensor(frame.values, requires_grad=True)
+
+optimization_models = {}
+all_models = {}
+
+#This is a modular function for iterative exploration of hyperparameters. 
+#It outputs all the results to two separate dictionaries for graphing model performance and cherry picking best models.
+def optimize(training_data, validation_data, num_samples, training_count):
+	cols = len(training_data.columns)-1
+	validation_subset = validation_data#.sample(2048)
+	validation_targets = convert_frame(validation_subset["TARGET"]).long()
+	validation_subset = convert_frame(validation_subset.drop(["TARGET"], axis=1)).float()
+	
+	params = {}
+	params["rate"] = list(10**np.random.uniform(-3, -1, 5))
+	params["reg"] = list(10**np.random.uniform(-5, -3, 5))
+	params["batch_size"] = [4096]	
+	params["epochs"] = [5]
+	params["num_layers"] = [4]
+	params["layer_size"] = [150, 300]
+	params["lr_decay"] = [0.9]
+	params["pruning"] = [0.8]
+	params["target_weight"] = [5.0]
+	
+	combinations = itertools.product(*params.values())
+	total_counts = len(list(combinations))
+	combinations = itertools.product(*params.values())
+	print(total_counts)
+	for model_num, param_set in enumerate(list(combinations)):
+		start = time.time()
+		param_dict = {}
+		for index, param in enumerate(param_set):
+			param_dict[list(params.keys())[index]] = param
+		
+		if param_dict["reg"] > param_dict["rate"]:
+			continue
+		model = net(num_features = cols, 
+		   params = param_dict,
+		   verbose=False)
+		
+		for sample_num in range(num_samples):
+			training_subset = training_data.sample(training_count)
+			training_targets = training_subset["TARGET"]
+			training_subset = training_subset.drop(["TARGET"], axis=1)
+			model.train(training_subset, training_targets, validation_subset, validation_targets)
+			
+		print("[{}/{}] Time Elapsed: {:d}s Training Loss: {:.4f} Validation Loss: {:.4f}".format(model_num+1, total_counts, int(time.time()-start), model.loss_history[-1], model.validation_loss[-1]))
+		
+		equal = model.target_accuracy > 0.6 and model.nontarget_accuracy > 0.6
+		reliable = model.nontarget_accuracy > 0.9 and model.target_accuracy > 0.2
+		if True or (equal or reliable) and model.performance > 1.2:
+			run_models({(param_dict["rate"], param_dict["reg"]): model}, validation_data, verbose = False)
+			equal = model.target_accuracy > 0.6 and model.nontarget_accuracy > 0.6
+			reliable = model.nontarget_accuracy > 0.8 and model.target_accuracy > 0.2
+			if True or (equal or reliable) and model.performance > 1.2:
+				print(("    ").join(["{}: {:.9}".format(key, str(value if type(value) != np.float64 else f'{value:.2e}')) for key, value in param_dict.items()]))
+				print("Correct Targets: {:.2f}%".format(100*model.target_accuracy))
+				print("Correct Nontargets: {:.2f}%".format(100*model.nontarget_accuracy))
+				print("=======================================")
+				optimization_models[param_set] = model
+		all_models[param_set] = model
+
+#This function accepts the raw data provided for the project and converts it into a format that can be consumed.
+#It aggregates data from all tables and joins them with the main data set.
+#A section of data is segmented off for use as model validation for determining generalizability.
+def fix_data(training_data, testing_data, validation_size = 0.1):
+	training_data = training_data.copy()
+	testing_data = testing_data.copy()
+	tables = FieldCalcs.get_calculated_fields()
+	for name, table in tables.items():
+		table.columns = [f"{name}_{column}" for column in table.columns]
+		
+	datasets = {"training_data": training_data, "testing_data": testing_data}
+	for dataset, data in datasets.items():
+		dummies = []
+		for name, table in tables.items():
+			data = pd.merge(data, table, on="SK_ID_CURR", how="left")
+			for column in table.columns:
+				if "distribution" in column:
+					continue
+				if table[column].dtype != object:
+					data[f"flag_{column}"] = np.where((~data[column].isnull() & data[column] != 0), 1, 0)
+		data["total_debts"] = pd.merge(data[["SK_ID_CURR", "AMT_ANNUITY"]], tables["total_debts"].fillna(0), on="SK_ID_CURR", how ="left").iloc[:, 1:].sum(axis=1)
+		data["debt_to_income"] = data["total_debts"] / data["AMT_INCOME_TOTAL"]
+		
+		data = data.drop(["SK_ID_CURR"], axis=1)
+		for column in data.columns:
+			if column == "TARGET":
+				continue
+			if data[column].dtype == object or (data[column].dtype == np.int64 and column[0:3] != "CNT" and column[0:4] != "DAYS"):
+				dummies.append(column)
+		
+		for column in data.columns:
+			if data[column].isnull().any():
+				data["nan_"+column] = np.where(data[column].isnull(), 1, 0)
+				if column not in dummies:
+					data[column] = data[column].fillna(0)
+		
+		datasets[dataset] = pd.get_dummies(data, columns=dummies)
+		
+		
+	
+	cols = datasets["training_data"].columns.union(datasets["testing_data"].columns)
+	training_data = datasets["training_data"].reindex(cols, axis=1, fill_value=0)#.astype(np.float64)
+	testing_data = datasets["testing_data"].reindex(cols, axis=1, fill_value=0)#.astype(np.float64)
+	testing_data = testing_data.drop(columns=["TARGET"], axis=1)
+	
+	cutoff = int(len(training_data)* (1-validation_size))
+	validation_data = training_data[cutoff:]
+	training_data = training_data[:cutoff]
+	
+	return training_data, validation_data, testing_data, tables
+	
+
+
+#=============================================================
+#==================== Bureau Calculations ====================
+#=============================================================
+
+#All data engineering occurs here. Tables are queried for merging.
+class FieldCalcs:
+	
+	def __init__(self):
+		pass
+	
+	@staticmethod
+	def get_calculated_fields():
+		tables = {}
+		aggregated_bureau = pd.DataFrame(np.unique(data_bureau["SK_ID_CURR"]), index = np.unique(data_bureau["SK_ID_CURR"]), columns = ["SK_ID_CURR"])
+		
+		
+		tables["most_common_credit_type"] = data_bureau[["SK_ID_CURR", "CREDIT_TYPE"]].groupby(["SK_ID_CURR"], as_index = True).agg(pd.Series.mode).apply(lambda x: x[0] if type(x[0]) != np.ndarray else x[0][0], axis=1)
+		tables["most_common_credit_type"] = pd.DataFrame(tables["most_common_credit_type"], columns=["credit_type"])
+		tables["bureau_loans"] = data_bureau.groupby(["SK_ID_CURR", "CREDIT_ACTIVE"], as_index=True)[["CREDIT_ACTIVE"]].count().unstack(level=1).fillna(0)
+		tables["bureau_loan_totals"] = data_bureau.groupby(["SK_ID_CURR"], as_index=True)[["SK_ID_CURR"]].count()
+		tables["bureau_status_distribution"] = tables["bureau_loans"]/tables["bureau_loan_totals"].values
+		
+		#============================================================
+		#================= Home Credit Calculations =================
+		#============================================================
+		
+		
+		tables["credit_loans"] = data_previous.groupby(["SK_ID_CURR", "NAME_CONTRACT_STATUS"], as_index=True)[["SK_ID_CURR"]].count().unstack(level=1)
+		tables["credit_loans"].columns = tables["credit_loans"].columns.droplevel(0)
+		tables["total_loans"] = data_previous.groupby(["SK_ID_CURR"], as_index=True)[["SK_ID_CURR"]].count()
+		tables["credit_loan_distribution"] = tables["credit_loans"] / tables["total_loans"].values
+		
+		rejected_priors = data_previous[(data_previous["CODE_REJECT_REASON"] != "XAP") & (data_previous["CODE_REJECT_REASON"] != "XNA")]
+		
+		#merge the followingwF
+		tables["rejected_loans"] = rejected_priors.groupby(["SK_ID_CURR", "CODE_REJECT_REASON"], as_index=True)[["SK_ID_CURR"]].count().unstack(level=1).fillna(0)
+		tables["rejected_loans"].columns = tables["rejected_loans"].columns.droplevel(0)
+		tables["total_rejected_loans"] = rejected_priors.groupby(["SK_ID_CURR"], as_index=True)[["SK_ID_CURR"]].count()
+		tables["rejected_loan_distribution"] = tables["rejected_loans"] / tables["total_rejected_loans"].values
+		
+		#============================================================
+	 	#=================== Debt to Income Ratio ===================
+	 	#============================================================
+		
+		bureau_debts = data_bureau[data_bureau["CREDIT_ACTIVE"] == "Active"]
+		bureau_debts = bureau_debts.groupby("SK_ID_CURR")[["AMT_ANNUITY"]].sum()
+		
+		payments = data_cash.groupby(["SK_ID_CURR", "SK_ID_PREV"])[["MONTHS_BALANCE"]].max().reset_index()
+		prior_loans = pd.merge(payments, data_cash[["SK_ID_PREV", "NAME_CONTRACT_STATUS", "MONTHS_BALANCE"]], on=["SK_ID_PREV", "MONTHS_BALANCE"], how="left")
+		active_priors = prior_loans[prior_loans.NAME_CONTRACT_STATUS == "Active"]
+		#most_recent = data_payments.groupby(["SK_ID_PREV"])[["NUM_INSTALMENT_NUMBER"]].max()
+		#most_recent = pd.merge(most_recent, data_payments[["SK_ID_PREV", "NUM_INSTALMENT_NUMBER", "AMT_INSTALMENT"]], on=["SK_ID_PREV", "NUM_INSTALMENT_NUMBER"], how ="left")
+		
+		active_debts = pd.merge(active_priors, data_previous[["SK_ID_PREV", "AMT_ANNUITY"]], on="SK_ID_PREV", how="left")
+		active_debts = active_debts.groupby(["SK_ID_CURR"])[["AMT_ANNUITY"]].sum()
+		 
+		tables["total_debts"] = pd.merge(bureau_debts, active_debts, on="SK_ID_CURR", how="outer")
+		
+		#categorical_columns = [col_name for col_name in col_names if col_name[0:8] == "category"]
+		#results = pd.get_dummies(results, columns = categorical_columns)
+		#results = results.drop(categorical_columns)
+		return tables
+
+#==================================================
+
+#Out of the cherry picked models from the optimize() function, they will be evaluated for best performance on validation data.
+def run_models(models, val_data, verbose = True):
+	val_targets = val_data["TARGET"]
+	val_data = val_data.drop(["TARGET"], axis=1)
+	
+	val_targets = convert_frame(val_targets).long()
+	val_data = convert_frame(val_data).float()
+	
+	nontargets = sum(val_targets==0)
+	targets = sum(val_targets == 1)
+	best_model = None
+
+	for settings, model in models.items():
+		scores = model.get_accuracy(val_data, val_targets)
+		target_scores = sum(x > 0.5 and y == 1 for x, y in zip(scores[:, 0], val_targets))
+		nontarget_scores = sum(x < 0.5 and y == 0 for x, y in zip(scores[:, 0], val_targets))
+		#target_false_positives = torch.sum(x==1 and y == 0 for x,y in zip(torch.max(scores, dim=1)[1], val_targets))
+		#nontarget_false_negatives = torch.sum(x==0 and y == 1 for x,y in zip(torch.max(scores, dim=1)[1], val_targets))
+		model.target_accuracy = target_scores/targets
+		model.nontarget_accuracy = nontarget_scores/nontargets
+		model.performance = (target_scores/targets)+(nontarget_scores/nontargets)
+		if verbose: 
+			print("Target percentage: {:.2f}%".format(100*model.target_accuracy))
+			print("Nontarget percentage: {:.2f}%".format(100*model.nontarget_accuracy))
+		if best_model == None or best_model.performance < model.performance:
+			best_model = model
+			best_model.settings = settings
+		
+	if verbose: 
+		print("Best Results:")
+		print("Target Accuracy: {:.2f}%".format(best_model.target_accuracy))
+		print("Nontarget Accuracy: {:.2f}%".format(best_model.nontarget_accuracy))
+	
+	return best_model
+
+#When a model is ready to run predictive analysis for final evaluation, this function will yield a csv containing applicant classifications.
+def print_submission(num, model, test_ids, test_data):
+	test_data = convert_frame(test_data).float()
+	scores = model.get_accuracy(test_data, targets = None, test=True)[:, 0].tolist()
+	#scores = np.argmax(scores, axis=1)
+	file = open(r"F:\Studying\Python\Kaggle Competition\submission for model {}.csv".format(num), "w")
+	file.write("SK_ID_CURR,TARGET\n")
+	for test_id, score in zip(test_ids, scores):
+		file.write("{},{}\n".format(test_id, score))
+		
+	file.close()
+
+
+#Raw data imports
+data_bureau = pd.read_csv(r"F:\Studying\Python\Kaggle Competition\bureau.csv")
+data_balance = pd.read_csv(r"F:\Studying\Python\Kaggle Competition\bureau_balance.csv")
+data_credit = pd.read_csv(r"F:\Studying\Python\Kaggle Competition\credit_card_balance.csv")
+data_payments = pd.read_csv(r"F:\Studying\Python\Kaggle Competition\installments_payments.csv")
+data_cash = pd.read_csv(r"F:\Studying\Python\Kaggle Competition\POS_CASH_balance.csv")
+data_previous = pd.read_csv(r"F:\Studying\Python\Kaggle Competition\previous_application.csv")
+data_train = pd.read_csv(r"F:\Studying\Python\Kaggle Competition\application_train.csv")
+data_test = pd.read_csv(r"F:\Studying\Python\Kaggle Competition\application_test.csv")
+
+
+#Final tables of parsed data inputs
+training_data, validation_data, testing_data, tables = fix_data(data_train, data_test, validation_size = 0.1)
+set_names = ["data_bureau", "data_balance", "data_credit", "data_payments", "data_cash", "data_previous"]
+datasets = [data_bureau, data_balance, data_credit, data_payments, data_cash, data_previous]
+
+usable_ids = []
+
+#Tool for visually evaluating resulting models performance in training via graph
+def print_loss_graph(results, title):
+	fig, ax = plt.subplots()
+	ax.set_title(title)
+	ax.set_ylim([0, 2])
+	for result in results:
+		model = results[result]
+		ax.plot(torch.tensor(model.loss_history).tolist(), color="tab:blue")
+		ax.plot(torch.tensor(model.validation_loss).tolist(), color="tab:orange")
+	fig
---
+++ neural_net.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+####==================================================================####
+####==============  NEURAL NETWORK IMPLEMENTATION BELOW ==============####
+####==================================================================####
+class net(nn.Module):
+	def __init__(self, 
+			  num_features,
+			  params, 
+			  verbose=False):
+		super(net, self).__init__()
+		self.model_params = {
+			"batch_size": 4096,
+			"num_layers": 4,
+			"epochs": 10,
+			"layer_size": 100,
+			"pruning": 1.0,
+			"target_weight": 11.5,
+			"rate": 0.001,
+			"reg": 0.001,
+			"lr_decay": 0.98,
+			"dropout": 0.3
+			}
+		self.verbose = verbose
+		for param, value in params.items():
+			if param in self.model_params.keys():
+				self.model_params[param] = value
+		self.target_accuracy = 0
+		self.nontarget_accuracy = 0
+		self.performance = 0
+		#print(cols)
+		
+		current_size = num_features
+		for number in np.arange(self.model_params["num_layers"]):
+			next_size = int(current_size * self.model_params["pruning"]) if self.model_params["pruning"] < 1.0 else self.model_params["layer_size"]
+			setattr(self, "batch{}".format(number), nn.BatchNorm1d(current_size, eps=1e-5, momentum=0.1, track_running_stats = True))
+			setattr(self, "linear{}".format(number), nn.Linear(current_size, next_size if number != np.arange(self.model_params["num_layers"])[-1] else 1))
+			setattr(self, "relu{}".format(number), nn.GELU())
+			current_size = next_size
+		
+		self.dropout = nn.Dropout(p=self.model_params["dropout"])
+		
+		#self.softmax = nn.Softmax(dim=1)
+		#self.loss_func = nn.CrossEntropyLoss(weight=torch.tensor([1, target_weight]).float())
+		self.optimizer = torch.optim.Adam(self.parameters(), lr = self.model_params["rate"], weight_decay = self.model_params["reg"])
+		self.scheduler = torch.optim.lr_scheduler.ExponentialLR(self.optimizer, self.model_params["lr_decay"])
+		self.sigmoid = nn.Sigmoid()
+		self.loss_func = nn.BCEWithLogitsLoss(pos_weight = torch.tensor([self.model_params["target_weight"]]).float())
+		self.loss_history = []
+		self.validation_loss = []
+		
+	
+	def forward(self, data, targets, test=False):
+		x = data
+		for i in np.arange(self.model_params["num_layers"]):
+			x = getattr(self, "batch"+str(i))(x)
+			if not test:
+				x = self.dropout(x)
+			x = getattr(self, "relu"+str(i))(x)
+			x = getattr(self, "linear"+str(i))(x)
+		if test:
+			return x, 0
+		loss = self.loss_func(x[:, 0], targets.float())
+		return x, loss
+
+	def train(self, data, targets, validation_subset, validation_targets):
+		training_set = torch.tensor(data.values, requires_grad = True).float()
+		training_targets_set = torch.tensor(targets.values, requires_grad = True).long()
+		
+		training_batches = torch.split(training_set, self.model_params["batch_size"])
+		target_batches = torch.split(training_targets_set, self.model_params["batch_size"])
+		
+		
+		for epoch in range(self.model_params["epochs"]):
+			if self.verbose: print("====== Epoch {} ======".format(epoch))
+
+			for set_num, training_batch, target_batch in zip(range(len(training_batches)), training_batches, target_batches):
+				
+				scores, loss = self.forward(training_batch, target_batch)
+				self.loss_history.append(loss)
+				with torch.no_grad():
+					self.validation_loss.append(self.forward(validation_subset, validation_targets)[1])
+				loss.backward()
+				self.optimizer.step()
+				self.optimizer.zero_grad()
+			self.scheduler.step()
+			
+				
+		with torch.no_grad():
+			scores = self.sigmoid(scores)
+			scores[:] = (scores > 0.5).int()
+
+			correct_targets = sum(x > 0.5 and y == 1 for x, y in zip(scores[:, 0], target_batch))
+			total_targets = sum(y==1 for y in target_batch)
+			target_accuracy = correct_targets/total_targets
+			
+			correct_nontargets = sum(x < 0.5 and y == 0 for x, y in zip(scores[:, 0], target_batch))
+			total_nontargets = sum(y==0 for y in target_batch)
+			nontarget_accuracy = correct_nontargets/total_nontargets
+			
+			performance = target_accuracy + nontarget_accuracy
+			
+			if performance > self.performance:
+				self.performance = performance
+				self.target_accuracy = target_accuracy
+				self.nontarget_accuracy = nontarget_accuracy
+			
+			if self.verbose: 
+				print("Loss = {:.3f}".format(self.loss))
+				print("Target percentage: {:.2f}%".format(100*self.target_accuracy))
+				print("Nontarget percentage: {:.2f}%".format(100*self.nontarget_accuracy))
+		if self.verbose: print("=======================================")
+			
+	
+	def get_accuracy(self, data, targets = None, test=False):
+		with torch.no_grad():
+			scores, loss = self.forward(data, targets, test=test)
+			#scores = torch.divide(torch.absolute(scores), torch.sum(torch.absolute(scores), axis=1, keepdim=True))
+			scores = self.sigmoid(scores)
+			if targets == None:
+				return scores
+			scores[:] = (scores > 0.5).int()
+		return scores
+
+	def print_scores(self, test_data):
+		self.get_accuracy()
+		return
+		pass
